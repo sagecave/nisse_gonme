9e54562afa9f42064caf2dd70deb2ae88bf38ae3
”sagecave” <Andreassage@gmail.com>
2024-11-29 08:35:06 +0100

    s

--- src/app/components/NisseCounter/Nisser.jsx
+++ src/app/components/NisseCounter/Nisser.jsx
@@ -2,7 +2,7 @@ const Nisser = () => {
   return (
     <form className="flex justify-center flex-col w-max h-max bg-red-500 p-5 rounded-lg" action="numOfNisser">
       <label for="nisser">Vælg antal nisser 1-10</label>
-      <input type="number" id="nisser" name="nisser" />
+      <input className="text-black" type="number" id="nisser" name="nisser" />
       <input type="submit" />
     </form>
   );
